d3f2861bd05dfd0d4f5fa50f9a2a2d5a40849009
Avymob Factory <factory@localhost>
2026-09-14 09:57:06 +0000

    Implement factory work item

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,16 @@
 from calculator import divide
+import pytest
 
-
-def test_divide():
+def test_divide_normal():
     assert divide(6, 2) == 3
+    assert divide(-6, 2) == -3
+    assert divide(6, -2) == -3
+    assert divide(6, 0.5) == 12
+    assert divide(7.5, 2.5) == 3
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError, match="Denominator must not be zero"):
+        divide(1, 0)
+    with pytest.raises(ValueError, match="Denominator must not be zero"):
+        divide(5.4, 0)
